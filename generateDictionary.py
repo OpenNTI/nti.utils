249ddb5acb61f2552f832f6ab18f068f6aa377fe
Jason Madden <jason.madden@nextthought.com>
2012-02-09 15:54:18 +0000

    Fix imports (and formatting) of generateDictionary to prevent spurious test failures.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@4983 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- generateDictionary.py
+++ generateDictionary.py
@@ -1,15 +1,20 @@
-import struct, os, cPickle
+import os, cPickle
 import xml.sax
 import sys
-import zlib
 import gzip
+from xml.sax.handler import ContentHandler
+# The pywikipedia dist has both a wiktionary package and a wiktionary module
+# one seems to be a duplicate of the other, not sure which is canonical
+# When installed with distutils, only the wiktionary module gets compiled and packaged
+# which means this is a class, not a sub-module. It's weird.
+from pywikipedia.wiktionary import WiktionaryPage
 
 def main(args):
 	action = args.pop(0)
 
 	location = args.pop(0)
 
-	wiki=Wiktionary(location)
+	wiki = Wiktionary(location)
 
 	if action == 'generate':
 		wiki.generateDictionary(args.pop(0))
@@ -80,56 +85,53 @@ def main(args):
 
 class Wiktionary(object):
 
-	indexName='dictionary.index.gz'
-	dictName='dictionary.bin.gz'
+	indexName = 'dictionary.index.gz'
+	dictName = 'dictionary.bin.gz'
 
-	index={}
-	dictFile=None
+	index = {}
+	dictFile = None
 
 	def __init__(self, location):
-		self.location=location
+		self.location = location
 		self.__loadDict()
 
 
 	def __loadDict(self):
 
-		indexPath=os.path.join(self.location, self.indexName)
+		indexPath = os.path.join(self.location, self.indexName)
 
 		if not os.path.exists(indexPath):
 			return
 
-		indexFile=gzip.open(os.path.join(self.location, self.indexName),'rb')
-		self.index=cPickle.load(indexFile)
+		indexFile = gzip.open(os.path.join(self.location, self.indexName),'rb')
+		self.index = cPickle.load(indexFile)
 		indexFile.close()
 
-		dictPath=os.path.join(self.location, self.dictName)
+		dictPath = os.path.join(self.location, self.dictName)
 
 		if os.path.exists(dictPath):
-			self.dictFile=dictPath
+			self.dictFile = dictPath
 		else:
-			self.indexFile={}
-			self.dictFile=None
+			self.indexFile = {}
+			self.dictFile = None
 
 
 
 	def generateDictionary(self, wiktionaryDump):
-
-
 		if not os.path.isdir(self.location):
 			os.makedirs(self.location)
 
+		dict_file = gzip.open(os.path.join(self.location, self.dictName), 'wb')
 
-		dict=gzip.open(os.path.join(self.location, self.dictName), 'wb')
-
-		parser=xml.sax.make_parser()
-		parser.setContentHandler(WiktionaryDumpHandler(self.index, dict))
-		dump=open(wiktionaryDump)
+		parser = xml.sax.make_parser()
+		parser.setContentHandler(WiktionaryDumpHandler(self.index, dict_file))
+		dump = open(wiktionaryDump)
 		parser.parse(dump)
 		dump.close()
 
-		dict.close()
+		dict_file.close()
 
-		indexFile=gzip.open(os.path.join(self.location,self.indexName), 'wb')
+		indexFile = gzip.open(os.path.join(self.location,self.indexName), 'wb')
 
 		cPickle.dump(self.index, indexFile)
 
@@ -142,79 +144,75 @@ class Wiktionary(object):
 		if term not in self.index:
 			return None
 
-		dictionary=gzip.open(self.dictFile, 'rb')
+		dictionary = gzip.open(self.dictFile, 'rb')
 		dictionary.seek(self.index[term])
 
-		res= cPickle.load(dictionary)
+		res = cPickle.load(dictionary)
 		dictionary.close()
 
 		return res
 
 
-
-from xml.sax.handler import ContentHandler
-
-from pywikipedia.wiktionary.wiktionarypage import *
-
 class WiktionaryDumpHandler(ContentHandler):
 
-	def __init__(self,index, dictionary):
-		self.index=index
-		self.dictionary=dictionary
+	def __init__(self, index, dictionary):
+		ContentHandler.__init__( self ) # Not a new-style class :(
+		self.index = index
+		self.dictionary = dictionary
 
-		self.name=u''
-		self.nameLabel='title'
-		self.insideName=False
+		self.name = u''
+		self.nameLabel = 'title'
+		self.insideName = False
 
-		self.markup=u''
-		self.markupLabel='text'
-		self.insideMarkup=False
+		self.markup = u''
+		self.markupLabel = 'text'
+		self.insideMarkup = False
 
-		self.pageLabel='page'
-		self.insidePage=False
+		self.pageLabel = 'page'
+		self.insidePage = False
 
-		self.page=0
+		self.page = 0
 
 	def startElement(self, localname, attrs):
 		if localname == self.pageLabel:
-			self.insidePage=True
-			self.page=self.page+1
-			print 'Found page %d'%self.page
+			self.insidePage = True
+			self.page = self.page + 1
+			print 'Found page %d' % self.page
 		elif localname == self.nameLabel:
-			self.insideName=True
-			self.name=''
+			self.insideName = True
+			self.name = ''
 		elif localname == self.markupLabel:
-			self.insideMarkup=True
-			self.markup=''
+			self.insideMarkup = True
+			self.markup = ''
 
 	def endElement(self, localname):
 		if localname == self.pageLabel:
-			self.insidePage=False
+			self.insidePage = False
 		elif localname == self.nameLabel:
-			self.insideName=False
+			self.insideName = False
 			#In the event that its not a word (based on the title) set that we are out of the page so we don't collect wikimarkup
 			if self.name.find(':')>-1:
-				self.name=''
-				self.insidePage=False
+				self.name = ''
+				self.insidePage = False
 		elif self.insidePage and localname == self.markupLabel:
-			self.insideMarkup=False
+			self.insideMarkup = False
 			self.persistEntry(self.name, self.markup)
 
 	def persistEntry(self,title, text):
 		if text.lower().find('==english==') < 0:
 			return
-		loc=self.dictionary.tell()
+		loc = self.dictionary.tell()
 		#cPickle.dump(text, self.dictionary)
-		page=WiktionaryPage('en', title)
+		page = WiktionaryPage('en', title)
 		page.parseWikiPage(text)
 		cPickle.dump(page, self.dictionary)
-		self.index[title]=loc
+		self.index[title] = loc
 
 	def characters(self, data):
 		if self.insidePage and self.insideName:
-			self.name=self.name+data
+			self.name = self.name+data
 		elif self.insidePage and self.insideMarkup:
-			self.markup=self.markup+data
+			self.markup = self.markup+data
 
 
 
