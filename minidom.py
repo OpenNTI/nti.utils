ff6dbeeb09471a627ece9940c317f1bf633568f6
Jason Madden <jason.madden@nextthought.com>
2012-04-28 23:03:47 +0000

    First part of rewriting the library handling code for consistency, flexibility and performance. It is not fully consistent with what the individual TOCs say. Needs more unittesting.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@6881 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ minidom.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Utilities for working with :mod:`xml.dom.minidom` objects.
+$Id$
+"""
+from __future__ import print_function, unicode_literals
+
+
+def minidom_writexml(document, outfile):
+	"""
+	Papers over some very bad Unicode issues
+	that crop up with xml.dom.minidom.
+	"""
+	class _StupidIOPackageCannotDealWithBothStrAndUnicodeObjects(object):
+		def __init__( self, under ):
+			self._under = under
+		def write( self, x ):
+			if isinstance( x, str ):
+				x = unicode(x)
+			self._under.write( x )
+	with io.open(outfile, "w", encoding='utf-8') as f:
+		document.writexml(_StupidIOPackageCannotDealWithBothStrAndUnicodeObjects(f),
+						  encoding=u'utf-8')
