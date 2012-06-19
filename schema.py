8cebc23419a44e66926e92f3aa4e475432f4f4af
Jason Madden <jason.madden@nextthought.com>
2012-06-19 17:20:26 +0000

    Start configuring (some) objects based off of their schemas, adding the capability to do evolution likewise, and replacing a bunch of ad-hoc code. Improvements to errors.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@8207 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ schema.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Utility classes and methods for working with zope schemas.
+
+Also patches a bug in the :class:`dm.zope.schema.schema.Object` class
+that requires the default value for ``check_declaration`` to be specified;
+thus always import `Object` from this module
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals
+
+from dm.zope.schema.schema import SchemaConfigured, schemadict, Object
+
+class PermissiveSchemaConfigured(SchemaConfigured):
+	"""
+	A mixin subclass of :class:`SchemaConfigured` that allows
+	for extra keywords (those not defined in the schema) to silently be ignored.
+	This is an aid to evolution of code and con be helpful in testing.
+
+	To allow for one-by-one conversions and updates, this class defines an attribute
+	``SC_PERMISSIVE``, defaulting to True, that controls this behaviour.
+	"""
+
+	SC_PERMISSIVE = True
+
+	def __init__( self, **kwargs ):
+		if not self.SC_PERMISSIVE:
+			super(PermissiveSchemaConfigured,self).__init__( **kwargs )
+		else:
+			schema = schemadict(self.sc_schema_spec())
+			for k in kwargs.keys():
+				if k not in schema:
+					kwargs.pop( k )
+			super(PermissiveSchemaConfigured,self).__init__( **kwargs )
+
+Object.check_declaration = True
