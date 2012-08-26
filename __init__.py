99a6a06945332bd0b5db159068893c36d1bfa01f
Jason Madden <jason.madden@nextthought.com>
2012-08-26 23:22:49 +0000

    Notes on an alternate, federated gravatar-like service and hosting software.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@10804 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -4,21 +4,31 @@ from __future__ import print_function, unicode_literals
 import hashlib
 import urlparse
 
+_AVATAR_SERVICES = { 'gravatar': { True: 'secure.gravatar.com',
+								   False: 'www.gravatar.com' },
+					'libravatar': {True: 'seccdn.libravatar.org',
+								   False: 'cdn.libravatar.org'} }
+
+
 def create_gravatar_url( username,
 						 defaultGravatarType='mm',
 						 secure=False,
-						 size=128 ):
+						 size=128,
+						 service='gravatar'):
 	"""
-	:return: A gravatar URL for the given username. See http://en.gravatar.com/site/implement/images/
+	Return a gravatar URL for the given username (which is assumed to be an email address).
+
 	:keyword basestring defaultGravatarType: The gravatar type to use if no specific
 		gravatar is available. Defaults to 'mm' for mystery man.
 	:keyword bool secure: If `True` (default `False`) HTTPS URL will be generated.
 	:keyword int size: The pixel dimensions of the image, between 1 and 512. Default 128.
+
+	:return: A gravatar URL for the given username. See http://en.gravatar.com/site/implement/images/
 	"""
 	md5str = hashlib.md5( username.lower() ).hexdigest()
 
 	scheme = 'https' if secure else 'http'
-	netloc = 'secure.gravatar.com' if secure else 'www.gravatar.com'
+	netloc = _AVATAR_SERVICES[service][secure]
 	path = '/avatar/' + md5str
 	params = ''
 	query = 's=%s&d=%s' % (size,defaultGravatarType)
