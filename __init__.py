ef88156895a6ad57ee84752083b3a93592b34377
Jason Madden <jason.madden@nextthought.com>
2012-08-29 18:14:31 +0000

    Provide storage for opt-in communication status. Allow avatar links to be edited. Provide avatar choices based on policy; these come back in preflight (assuming it passes and a Username is present) and also in the main User object once it exists.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@10965 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ from __future__ import print_function, unicode_literals
 import hashlib
 import urlparse
 
-_AVATAR_SERVICES = { 'gravatar': { True: 'secure.gravatar.com',
-								   False: 'www.gravatar.com' },
-					'libravatar': {True: 'seccdn.libravatar.org',
-								   False: 'cdn.libravatar.org'} }
+_AVATAR_SERVICES = { 'gravatar': { True: b'secure.gravatar.com',
+								   False: b'www.gravatar.com' },
+					'libravatar': {True: b'seccdn.libravatar.org',
+								   False: b'cdn.libravatar.org'} }
 
+GENERATED_GRAVATAR_TYPES = ('identicon', 'monsterid', 'wavatar', 'retro' )
+KNOWN_GRAVATAR_TYPES = GENERATED_GRAVATAR_TYPES + ('mm','404')
 
 def create_gravatar_url( username,
 						 defaultGravatarType='mm',
@@ -27,10 +29,10 @@ def create_gravatar_url( username,
 	"""
 	md5str = hashlib.md5( username.lower() ).hexdigest()
 
-	scheme = 'https' if secure else 'http'
+	scheme = b'https' if secure else b'http'
 	netloc = _AVATAR_SERVICES[service][secure]
-	path = '/avatar/' + md5str
+	path = b'/avatar/' + md5str
 	params = ''
-	query = 's=%s&d=%s' % (size,defaultGravatarType)
+	query = b's=%s&d=%s' % (size,defaultGravatarType)
 	fragment = ''
-	return urlparse.urlunparse( (scheme,netloc,path,params,query,fragment) )
+	return str(urlparse.urlunparse( (scheme,netloc,path,params,query,fragment) ))
