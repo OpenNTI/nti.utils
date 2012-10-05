a14882009b2bd4a39dbc0408c549bb144bd76c75
Jason Madden <jason.madden@nextthought.com>
2012-10-05 14:56:50 +0000

    enable the chameleon cache for the application. makes a big difference for email creation and the admin pages.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@12947 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals
 
+logger = __import__('logging').getLogger(__name__)
+
 import hashlib
 import urlparse
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
+
+from zope.dottedname import resolve as dottedname
 
 _AVATAR_SERVICES = { 'gravatar': { True: b'secure.gravatar.com',
 								   False: b'www.gravatar.com' },
@@ -40,3 +44,42 @@ def create_gravatar_url( username,
 	query = b's=%s&d=%s' % (size,defaultGravatarType)
 	fragment = ''
 	return str(urlparse.urlunparse( (scheme,netloc,path,params,query,fragment) ))
+
+
+import os
+def setupChameleonCache(config=False):
+	# Set up a cache for these things to make subsequent renders faster
+
+	def _make_cache_dir():
+		result = None
+		# In preference order
+		env_child = ( ('DATASERVER_ENV', ('var', 'chameleon_cache')),
+					  ('DATASERVER_DIR', ('var', 'chameleon_cache')),
+					  ('VIRTUAL_ENV', ('.chameleon_cache',) ) )
+		for env_var, child in env_child:
+			if env_var in os.environ:
+				parent = os.environ[env_var]
+				result = os.path.join( parent, *child )
+				break
+		if result is None:
+			result = os.path.join( os.getcwd(), '.chameleon_cache' )
+
+		logger.info( "Caching templates to %s", result )
+		try:
+			os.mkdir( result )
+		except OSError:
+			pass
+		return result
+
+	result = None
+	if not 'CHAMELEON_CACHE' in os.environ:
+		os.environ['CHAMELEON_CACHE'] = result = _make_cache_dir()
+	else:
+		result = os.environ['CHAMELEON_CACHE']
+
+	if config:
+		conf_mod = dottedname.resolve('chameleon.config')
+		if conf_mod.CACHE_DIRECTORY is None: # previously imported before we set the environment
+			result = conf_mod.CACHE_DIRECTORY = _make_cache_dir()
+
+	return result
