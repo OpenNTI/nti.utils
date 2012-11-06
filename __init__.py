4e5f9aa3c9df2f32ddcdeed2d2b8b247c1be5a49
Jason Madden <jason.madden@nextthought.com>
2012-11-06 16:34:52 +0000

    Share the cache location logic, and move them into the var dir of the environment. Production deployments on EC2 should make the var/caches/ dir a symlink to ephemeral storage, just like the var/log/ dir is.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@13760 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -47,39 +47,74 @@ def create_gravatar_url( username,
 
 
 import os
-def setupChameleonCache(config=False):
-	# Set up a cache for these things to make subsequent renders faster
 
-	def _make_cache_dir():
-		result = None
+def make_cache_dir(cache_name, env_var=None):
+	"""
+	Global utility to create and return a cache directory in the
+	most appropriate position.
+
+	This takes into account environment settings for an active application
+	server first. Following that, the python virtual installation is used, and
+	finally a system-specific cache location. If all that fails, and the current
+	working directory is writable, it will be used.
+
+	:param str cache_name: The specific (filesystem) name of the type of cache
+	:param str env_var: If given, then names an environment variable that will be
+		queried first. If the environment variable exists, then we will attempt to use
+		it in preference to anything else.
+	:return: A string giving a path to a directory for the cache.
+	:raises ValueError: If no cache location can be found.
+	"""
+
+	result = None
+
+	if env_var:
+		result = os.environ.get( env_var )
+
+	if result is None:
 		# In preference order
-		env_child = ( ('DATASERVER_ENV', ('var', 'chameleon_cache')),
-					  ('DATASERVER_DIR', ('var', 'chameleon_cache')),
-					  ('VIRTUAL_ENV', ('.chameleon_cache',) ) )
+		env_child = ( ('DATASERVER_ENV', ('var', 'caches', cache_name)),
+					  ('DATASERVER_DIR', ('var', 'caches', cache_name)),
+					  ('VIRTUAL_ENV',    ('var', 'caches', cache_name) ) )
 		for env_var, child in env_child:
 			if env_var in os.environ:
 				parent = os.environ[env_var]
 				result = os.path.join( parent, *child )
 				break
-		if result is None:
-			result = os.path.join( os.getcwd(), '.chameleon_cache' )
 
-		logger.info( "Caching templates to %s", result )
-		try:
-			os.mkdir( result )
-		except OSError:
-			pass
-		return result
+	# Ok, no environment to be found. How about some system specific stuff?
+	if result is None:
+		for system_loc in ( "~/Library/Caches/", # OS x
+							"~/.cache" ): # Linux
+			system_loc = os.path.expanduser( system_loc )
+			if os.path.isdir( system_loc ):
+				result = os.path.join( system_loc, "com.nextthought", "nti.dataserver", cache_name )
+				break
+
+	if result is None:
+		result = os.path.join( os.getcwd(), '.caches', cache_name )
+
+	try:
+		os.makedirs( result )
+	except OSError:
+		pass
+
+	if not os.path.isdir( result ):
+		raise ValueError( "Unable to find cache location for " + cache_name + " using " + result )
+	return result
+
+def setupChameleonCache(config=False):
+	# Set up a cache for these things to make subsequent renders faster
 
 	result = None
-	if not 'CHAMELEON_CACHE' in os.environ:
-		os.environ['CHAMELEON_CACHE'] = result = _make_cache_dir()
+	if not 'CHAMELEON_CACHE' in os.environ or not os.path.isdir( os.path.expanduser( os.environ['CHAMELEON_CACHE'] ) ):
+		os.environ['CHAMELEON_CACHE'] = result = make_cache_dir('chameleon_cache')
 	else:
 		result = os.environ['CHAMELEON_CACHE']
 
 	if config:
 		conf_mod = dottedname.resolve('chameleon.config')
 		if conf_mod.CACHE_DIRECTORY is None: # previously imported before we set the environment
-			result = conf_mod.CACHE_DIRECTORY = _make_cache_dir()
+			conf_mod.CACHE_DIRECTORY = result
 
 	return result
