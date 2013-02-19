36ebc9fd49cfed76b59a699245820489f677cdf6
Jason Madden <jason.madden@nextthought.com>
2013-02-19 23:55:23 +0000

    Work on generalizing and decoupling the note 'body' implementation for use in the new content types. This required generalizing some externalization rules and improving contentfragments coercions, all for the better. It also required fixing a censoring bug that lost interface information. Not completely finished, but close. Unit, gp and int tests all pass.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@16060 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- schema.py
+++ schema.py
@@ -11,7 +11,17 @@ $Id$
 """
 from __future__ import print_function, unicode_literals
 from . import MessageFactory as _
-from dm.zope.schema.schema import SchemaConfigured, schemadict, Object
+from dm.zope.schema.schema import SchemaConfigured, schemadict, Object as ObjectBase
+ObjectBase.check_declaration = True
+
+
+from zope import interface
+from zope import schema
+from zope import component
+from zope.component import handle
+from zope.event import notify
+from zope.schema import interfaces as sch_interfaces
+import numbers
 
 class PermissiveSchemaConfigured(SchemaConfigured):
 	"""
@@ -29,27 +39,11 @@ class PermissiveSchemaConfigured(SchemaConfigured):
 		if not self.SC_PERMISSIVE:
 			super(PermissiveSchemaConfigured,self).__init__( **kwargs )
 		else:
-			schema = schemadict(self.sc_schema_spec())
+			_schema = schemadict(self.sc_schema_spec())
 			for k in kwargs.keys():
-				if k not in schema:
+				if k not in _schema:
 					kwargs.pop( k )
 			super(PermissiveSchemaConfigured,self).__init__( **kwargs )
-
-Object.check_declaration = True
-
-from zope import interface
-from zope import schema
-from zope import component
-from zope.component import handle
-from zope.event import notify
-from zope.schema import interfaces as sch_interfaces
-import numbers
-
-class Number(schema.Float):
-	"""
-	A field that parses like a float from a string, but accepts any number.
-	"""
-	_type = numbers.Number
 
 class InvalidValue(sch_interfaces.InvalidValue):
 	"""
@@ -78,13 +72,19 @@ class FieldValidationMixin(object):
 	"""
 
 	def _fixup_validation_error_args( self, e, value ):
-		# Called when the exception has one argument, which is usually, thought not always,
+		# Called when the exception has one argument, which is usually, though not always,
 		# the message
 		e.args = (value, e.args[0], self.__name__)
+
+	def _fixup_validation_error_no_args(self, e, value ):
+		# Called when there are no arguments
+		e.args = (value, str(e), self.__name__ )
 
 	def _reraise_validation_error(self, e, value, _raise=False):
 		if len(e.args) == 1: # typically the message is the only thing
 			self._fixup_validation_error_args( e, value )
+		elif len(e.args) == 0: # Typically a SchemaNotProvided. Grr.
+			self._fixup_validation_error_no_args( e, value )
 		elif isinstance( e, sch_interfaces.TooShort ) and len(e.args) == 2:
 			# Note we're capitalizing the field in the message.
 			e.i18n_message = _('${field} is too short.', mapping={'field': self.__name__.capitalize(), 'minLength': e.args[1]})
@@ -103,6 +103,106 @@ class FieldValidationMixin(object):
 			super(FieldValidationMixin,self)._validate( value )
 		except sch_interfaces.ValidationError as e:
 			self._reraise_validation_error( e, value )
+
+class Object(FieldValidationMixin,ObjectBase):
+
+	def _fixup_validation_error_no_args(self, e, value ):
+		e.args = (value, e.__doc__, self.__name__, self.schema, list(interface.providedBy( value ) ))
+
+
+class ObjectOr(FieldValidationMixin,schema.Field):
+	"""
+	Similar to :class:`zope.schema.Object`, but accepts one of many non-overlapping
+	interfaces.
+	"""
+
+	def __init__( self, fields, **kwargs ):
+		"""
+		:param fields: A list or tuple of :class:`zope.schema.Object` instances.
+
+		"""
+		if not fields or not all( (sch_interfaces.IObject.providedBy( x ) for x in fields ) ):
+			raise sch_interfaces.WrongType()
+
+		self.fields = list(fields)
+		super(ObjectOr,self).__init__( **kwargs )
+
+		if self.__name__:
+			for field in self.fields:
+				field.__name__ = field.__name__ or self.__name__
+
+	def bind( self, obj ):
+		clone = super(ObjectOr,self).bind( obj )
+		clone.fields = [x.bind( obj ) for x in clone.fields]
+		return clone
+
+	def _validate( self, value ):
+		super(ObjectOr,self)._validate( value )
+		for field in self.fields:
+			try:
+				field.validate( value )
+				# one of them accepted, yay!
+				return
+			except sch_interfaces.ValidationError as e:
+				pass
+		# We get here only by all of them throwing an exception.
+		# we re-raise the last thing thrown
+		self._reraise_validation_error( e, value )
+
+	def fromObject( self, obj ):
+		"""
+		Similar to `fromUnicode`, attempts to turn the given object into something
+		acceptable and valid for this field. Raises a TypeError, ValueError, or
+		schema ValidationError if this cannot be done. Adaptation is attempted in the order
+		in which fields were given to the constructor.
+		"""
+
+		for field in self.fields:
+			try:
+				# Try to convert and validate
+				adapted = field.schema( obj )
+			except (TypeError, sch_interfaces.ValidationError):
+				# Nope, no good
+				pass
+			else:
+				# We got one that like the type. Do the validation
+				# now, and then return. Don't try to convert with others;
+				# this is probably our best error
+				try:
+					field.validate( adapted )
+					return adapted
+				except sch_interfaces.SchemaNotProvided:
+					# Except in one case. Some schema provides adapt to something
+					# that they do not actually want (e.g., ISanitizedHTMLContent can adapt as IPlainText when empty)
+					# so ignore that and keep trying
+					pass
+
+		# We get here if nothing worked and re-raise the last exception
+		raise
+
+
+class ObjectLen(FieldValidationMixin,schema.MinMaxLen,ObjectBase): # order matters
+	"""
+	Allows specifying a length for arbitrary object fields (though the
+	objects themselves must support the `len` function.
+	"""
+
+	def __init__( self, sch, min_length=0, max_length=None, **kwargs ):
+		# match the calling sequence of Object, which uses a non-keyword
+		# argument for schema.
+		# But to work with the superclass, we have to pass it as a keyword arg.
+		# it's weird.
+		super(ObjectLen,self).__init__( schema=sch, min_length=min_length, max_length=max_length, **kwargs )
+
+	def _fixup_validation_error_no_args(self, e, value ):
+		e.args = (value, e.__doc__, self.__name__, self.schema, list(interface.providedBy( value ) ))
+
+
+class Number(FieldValidationMixin,schema.Float):
+	"""
+	A field that parses like a float from a string, but accepts any number.
+	"""
+	_type = numbers.Number
 
 
 from zope.schema._field import BeforeObjectAssignedEvent
@@ -168,7 +268,7 @@ class HTTPURL(FieldValidationMixin,schema.URI):
 
 		return result
 
-class IndexedIterable(schema.List):
+class IndexedIterable(FieldValidationMixin,schema.List):
 	"""
 	An arbitrary (indexable) iterable, not necessarily a list or tuple;
 	either of those would be acceptable at any time.
@@ -176,7 +276,10 @@ class IndexedIterable(schema.List):
 	"""
 	_type = None # Override from super to not force a list
 
-class UniqueIterable(schema.Set):
+class ListOrTuple(IndexedIterable):
+	_type = (list,tuple)
+
+class UniqueIterable(FieldValidationMixin,schema.Set):
 	"""
 	An arbitrary iterable, not necessarily an actual :class:`set` object and
 	not necessarily iterable, but one whose contents are unique.
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -23,7 +23,10 @@ from nose.tools import assert_raises
 
 import nti.tests
 
-from nti.utils.schema import HTTPURL
+from nti.utils.schema import HTTPURL, ObjectOr, ObjectLen, Object
+from dolmen.builtins import IUnicode
+from zope.interface.common import interfaces as cmn_interfaces
+from zope.schema import interfaces as sch_interfaces
 from zope.schema.interfaces import InvalidURI
 
 def test_http_url():
@@ -43,3 +46,38 @@ def test_http_url():
 	assert_that( ex.exception, has_property( 'field', http ) )
 	assert_that( ex.exception, has_property( 'value', 'mailto:jason@nextthought.com' ) )
 	assert_that( ex.exception, has_property( 'message', 'The specified URI is not valid.' ) )
+
+
+
+def test_objector( ):
+
+	syntax_or_lookup = ObjectOr( (Object(cmn_interfaces.ISyntaxError),Object(cmn_interfaces.ILookupError), Object(IUnicode)) )
+
+	# validates
+	syntax_or_lookup.validate( SyntaxError() )
+	syntax_or_lookup.validate( LookupError() )
+
+	# doesn't validate
+	with assert_raises( sch_interfaces.ValidationError ):
+		syntax_or_lookup.validate( b'foo' )
+
+
+	assert_that( syntax_or_lookup.fromObject( 'foo' ), is_( 'foo' ) )
+
+	with assert_raises( TypeError ):
+		syntax_or_lookup.fromObject( object() )
+
+def test_objectlen():
+	# If we have the inheritance messed up, we will have problems
+	# creating, or we will have problems validating one part or the other.
+
+	olen = ObjectLen( IUnicode, max_length=5 ) # default val for min_length
+
+	olen.validate( 'a' )
+	olen.validate( '' )
+
+	with assert_raises( sch_interfaces.SchemaNotProvided ):
+		olen.validate( object() )
+
+	with assert_raises( sch_interfaces.TooLong ):
+		olen.validate( 'abcdef' )
