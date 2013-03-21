94da8d4f65d8366f6722b4d91bdf8ad32cb784f8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-21 20:38:16 +0000

    separte to graphics module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@17399 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- math.py
+++ math.py
@@ -7,116 +7,11 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-import math
-
-class AffineMatrix(object):
-
-	def __init__(self, a=None, b=None, c=None, d=None, tx=None, ty=None):
-		a = 1.0 if a is None else a
-		b = 0.0 if b is None else b
-		c = 0.0 if c is None else c
-		d = 1.0 if d is None else d
-		tx = 0.0 if tx is None else tx
-		ty = 0.0 if ty is None else ty
-		self.m = [a, b, c, d, tx, ty]
-
-	@property
-	def tx(self):
-		return self.m[4]
-
-	@property
-	def ty(self):
-		return self.m[5]
-
-	def reset(self):
-		self.m = [1.0, 0.0, 0.0 , 1.0, 0.0, 0.0]
-
-	def multiply(self, matrix):
-		m11 = self.m[0] * matrix.m[0] + self.m[2] * matrix.m[1]
-		m12 = self.m[1] * matrix.m[0] + self.m[3] * matrix.m[1]
-
-		m21 = self.m[0] * matrix.m[2] + self.m[2] * matrix.m[3]
-		m22 = self.m[1] * matrix.m[2] + self.m[3] * matrix.m[3]
-
-		dx = self.m[0] * matrix.m[4] + self.m[2] * matrix.m[5] + self.m[4]
-		dy = self.m[1] * matrix.m[4] + self.m[3] * matrix.m[5] + self.m[5]
-
-		self.m[0] = m11
-		self.m[1] = m12
-		self.m[2] = m21
-		self.m[3] = m22
-		self.m[4] = dx
-		self.m[5] = dy
-
-	def rotate(self, rad):
-		c = math.cos(rad)
-		s = math.sin(rad)
-		self.multiply(*[c, s, -s, c, 0.0, 0.0])
-
-	def translate(self, x, y=None):
-		if isinstance(x, (list, tuple)):
-			y = x[1]
-			x = x[0]
-
-		self.multiply(*[1.0 , 0.0, 0.0, 1.0, x, y])
-
-	def scale(self, sx, sy=None):
-		if isinstance(sx, (list, tuple)):
-			sx = sx[0]
-			sy = sx[1]
-
-		sy = sx is sy if None else sy
-		self.multiply(*[sx, 0.0, 0.0, sy, 0.0, 0.0])
-
-	def scale_all(self, scale):
-		i = len(self.m) - 1
-		while (i >= 0):
-			self.m[i] *= scale
-			i = i - 1
-	scaleAll = scale_all
-
-	def get_scale(self, averaged=False):
-		m = self.m
-		a = m[0]
-		b = m[1]
-		c = m[2]
-		d = m[3]
-		sx = math.sqrt(a * a + b * b)  # *(a<0? -1 : 1)
-		sy = math.sqrt(c * c + d * d)  # *(d<0? -1 : 1)
-		return (sx + sy) / 2.0 if averaged else [sx, sy]
-
-	getScale = get_scale
-
-	def get_rotation(self):
-		m = self.m
-		a = m[0]
-		b = m[1]
-		return math.atan2(b, a)
-
-	getRotation = get_rotation
-
-	def get_translation(self):
-		return [self.m[4], self.m[5]]
-
-	getTranslation = get_translation
-
-	def transform_point(self, px, py=None):
-		if isinstance(px, (list, tuple)):
-			px = px[0]
-			py = px[1]
-
-		return [
-			px * self.m[0] + py * self.m[2] + self.m[4],  # x
-			px * self.m[1] + py * self.m[3] + self.m[5]  # y
-		]
-
-	transformPoint = transform_point
-
-NTMatrix = AffineMatrix
+import numpy as np
 
 def choose(n, k, *args):
 	"""
-	A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
+	A fast way to calculate binomial coefficients.
 	"""
 	if 0 <= k <= n:
 		ntok = 1
@@ -129,7 +24,7 @@ def choose(n, k, *args):
 	else:
 		return 0
 
-def comb(N, k, *args):
+def combinations(N, k, *args):
 	"""
 	The number of combinations of N things taken k at a time
 	"""
@@ -146,3 +41,41 @@ def comb(N, k, *args):
 		val /= n
 		n += 1
 	return val
+
+try:
+	from scipy.misc import comb
+except ImportError:
+	comb = choose
+
+def bernstein_poly(i, n, t):
+	"""
+	 The Bernstein polynomial of n, i as a function of t
+	"""
+	return comb(n, i) * (t ** (n - i)) * (1 - t) ** i
+
+def bezier_curve(x_points, y_points, nTimes=1000):
+	"""
+   Given a set of control points, return the
+   bezier curve defined by the control points.
+
+   points should be a list of lists, or list of tuples
+   such as [ [1,1], 
+			 [2,3], 
+			 [4,5], ..[Xn, Yn] ]
+	nTimes is the number of time steps, defaults to 1000
+
+	See http://processingjs.nihongoresources.com/bezierinfo/
+	"""
+
+	nPoints = len(x_points)
+	xPoints = np.array(x_points)
+	yPoints = np.array(y_points)
+
+	t = np.linspace(0.0, 1.0, nTimes)
+
+	polynomial_array = np.array([ bernstein_poly(i, nPoints - 1, t) for i in range(0, nPoints)   ])
+
+	xvals = np.dot(xPoints, polynomial_array)
+	yvals = np.dot(yPoints, polynomial_array)
+
+	return xvals, yvals
