61eb3509c3a6ec2c6cf8551168274747a2cc38b3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-17 06:54:58 +0000

    add helper module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/utils@51427 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
-# $Id$
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
--- tests/test_dataurl.py
+++ tests/test_dataurl.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-$Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
-
-
-from hamcrest import assert_that
-from hamcrest import is_
-from hamcrest import has_property
-from hamcrest import is_not
 from hamcrest import none
+from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_property
 
 from ..dataurl import DataURL
-
 
 GIF_DATAURL = b'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw=='
 
