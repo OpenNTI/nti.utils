6374528e302c30f45f77e784292451130b97df32
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-24 20:57:36 +0000

    set deps
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@58271 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ py_impl = getattr(platform, 'python_implementation', lambda: None)
 IS_PYPY = py_impl() == 'PyPy'
 
 setup(
-    name='nti.store',
+    name='nti.utils',
     version=VERSION,
     author='Jason Madden',
     author_email='jason@nextthought.com',
@@ -34,7 +34,41 @@ setup(
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
 	install_requires=[
-		'setuptools'
+		'setuptools',
+        'Acquisition' if not IS_PYPY else '',
+        'Babel',
+        'ExtensionClass',
+        'ZODB',
+        'brownie',
+        'dm.transaction.aborthook',
+        'futures',
+        'gevent' if not IS_PYPY else '',
+        'greenlet' if not IS_PYPY else '', # pypy has its own greenlet implementation
+        'lingua',
+        'numpy' if not IS_PYPY else '',
+        'perfmetrics',
+        'plone.i18n',
+        'pycrypto' if not IS_PYPY else '',
+         # 'scipy' if not IS_PYPY else '', # don't include it yet
+        'transaction',
+        'zope.annotation',
+        'zope.browserresource',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.configuration',
+        'zope.container',
+        'zope.deferredimport',
+        'zope.deprecation',
+        'zope.dottedname',
+        'zope.interface',
+        'zope.schema',
+        'zope.vocabularyregistry',
+         # nti deps
+        'nti.schema',
+        'pywikipedia',
 	],
+    dependency_links=[
+        'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema'
+    ],
 	entry_points=entry_points
 )
