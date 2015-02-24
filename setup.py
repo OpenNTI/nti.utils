62945e8f46e1b2a9e875610693f7766870cdf933
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-24 17:13:01 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@60093 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-    'console_scripts': [
-    ],
+	'console_scripts': [
+	],
 }
 
 import platform
@@ -13,76 +13,76 @@ py_impl = getattr(platform, 'python_implementation', lambda: None)
 IS_PYPY = py_impl() == 'PyPy'
 
 TESTS_REQUIRE = [
-    'nose',
-    'nose2[coverage_plugin]',
-    'nose-timer',
-    'nose-progressive',
-    'nose-pudb',
-    'pyhamcrest',
-    'zope.testing',
-    'nti.testing',
-    'nti.nose_traceback_info',
+	'nose',
+	'nose2[coverage_plugin]',
+	'nose-timer',
+	'nose-progressive',
+	'nose-pudb',
+	'pyhamcrest',
+	'zope.testing',
+	'nti.testing'
 ]
 
 setup(
-    name='nti.utils',
-    version=VERSION,
-    author='Jason Madden',
-    author_email='jason@nextthought.com',
-    description="NTI Utils",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='utils',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.utils',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI Utils",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='utils',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: Implementation :: CPython',
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
-    tests_require=TESTS_REQUIRE,
+	tests_require=TESTS_REQUIRE,
 	install_requires=[
 		'setuptools',
-        'Acquisition' if not IS_PYPY else '',
-        'Babel',
-        'ExtensionClass',
-        'ZODB',
-        'dm.transaction.aborthook',
-        'futures',
-        'gevent' if not IS_PYPY else '',
-        'greenlet' if not IS_PYPY else '', # pypy has its own greenlet implementation
-        'lingua',
-        'numpy' if not IS_PYPY else '',
-        'perfmetrics',
-        'plone.i18n',
-        'pycrypto' if not IS_PYPY else '',
-         # 'scipy' if not IS_PYPY else '', # don't include it yet
-        'transaction',
-        'z3c.baseregistry',
-        'zope.annotation',
-        'zope.browserresource',
-        'zope.component',
-        'zope.configuration',
-        'zope.container',
-        'zope.deferredimport',
-        'zope.deprecation',
-        'zope.interface',
-        'zope.proxy',
-        'zope.schema',
-        'zope.vocabularyregistry',
-         # nti deps
-        'nti.common',
-        'nti.schema',
-        'pywikipedia',
+		'Acquisition' if not IS_PYPY else '',
+		'Babel',
+		'dm.transaction.aborthook',
+		'ExtensionClass',
+		'futures',
+		'gevent' if not IS_PYPY else '',
+		'greenlet' if not IS_PYPY else '',
+		'lingua',
+		'numpy' if not IS_PYPY else '',
+		'perfmetrics',
+		'plone.i18n',
+		'pycrypto' if not IS_PYPY else '',
+		'transaction',
+		'z3c.baseregistry',
+		'ZODB',
+		'zope.annotation',
+		'zope.browserresource',
+		'zope.component',
+		'zope.configuration',
+		'zope.container',
+		'zope.deferredimport',
+		'zope.deprecation',
+		'zope.interface',
+		'zope.proxy',
+		'zope.schema',
+		'zope.vocabularyregistry',
+		'nti.common',
+		'nti.schema',
+		'nti.nose_traceback_info',
+		'pywikipedia',
 	],
-    dependency_links=[
-        'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
-        'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
-    ],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+	],
 	entry_points=entry_points
 )
