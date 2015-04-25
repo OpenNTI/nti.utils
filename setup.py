b781f8a4a632ea522d684d691cbbdf92a933a6e5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-25 19:47:30 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@63907 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setup(
 		'test': TESTS_REQUIRE,
 	},
 	dependency_links=[
-		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
 		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
 	],
 	entry_points=entry_points
