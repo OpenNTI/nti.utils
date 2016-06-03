b5ceef212b30c45140244da7d7c791dcbfd0e2f8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-06-03 02:20:12 +0000

    check version of nameparser
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@89773 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/nameparser.py
+++ src/nti/utils/nameparser.py
@@ -11,6 +11,7 @@ logger = __import__('logging').getLogger(__name__)
 
 import nameparser
 nameparser_config = getattr(nameparser, "config")
+version_info = getattr(nameparser, 'VERSION', (0, 3, 16))
 
 def suffixes():
 	return set(getattr(nameparser_config, 'SUFFIXES', None) or ())
@@ -28,11 +29,10 @@ def all_suffixes():
 	return suffixes() | suffix_acronyms() | suffix_not_acronyms()
 
 def constants(prefixes=(), extra_suffixes=()):
-	v3_suffixes = suffixes()  # nameparser 0.3.X
 	prefixes = all_prefixes() if not prefixes else prefixes
 	extra_suffixes = () if not extra_suffixes else extra_suffixes
-	if v3_suffixes:  # nameparser 0.3.X
-		v3_suffixes = v3_suffixes | set(extra_suffixes)
+	if version_info[0] == 0 and version_info[1] <= 3: # nameparser 0.3.X
+		v3_suffixes = suffixes() | set(extra_suffixes)
 		constants = nameparser_config.Constants(prefixes=prefixes, suffixes=v3_suffixes)
 	else:
 		not_acronyms = suffix_not_acronyms()
