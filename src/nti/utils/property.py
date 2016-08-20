b45541a01bcac8cbc311d8c9863889f98e011123
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-08-20 22:56:43 +0000

    annotation_alias property to new pkg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@94834 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/property.py
+++ src/nti/utils/property.py
@@ -11,55 +11,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import operator
-
-from zope.annotation.interfaces import IAnnotations
-
-def annotation_alias(annotation_name, annotation_property=None, default=None, 
-					 delete=False, delete_quiet=True, doc=None):
-	"""
-	Returns a property that is a read/write alias for
-	a value stored as a :class:`zope.annotation.interface.IAnnotations`.
-
-	The object itself may be adaptable to an IAnnotations, or a property
-	of the object may be what is adaptable to the annotation. The later is intended
-	for use in adapters when the context object is what should be adapted.
-
-	:keyword bool delete: If ``True`` (not the default), then the property can be used
-		to delete the annotation.
-	:keyword bool delete_quiet: If ``True`` and `delete` is also True, then the property
-		will ignore key errors when deleting the annotation value.
-	:keyword str annotation_property: If set to a string, it is this property
-		of the object that will be adapted to IAnnotations. Most often this will
-		be ``context`` when used inside an adapter.
-	"""
-
-	if doc is None:
-		doc = 'Alias for annotation ' + annotation_name
-
-	factory = IAnnotations
-	if annotation_property:
-		factory = lambda self: IAnnotations( getattr( self, annotation_property ) )
-
-	fget = lambda self: factory(self).get(annotation_name, default)
-	fset = lambda self, nv: operator.setitem( factory(self), annotation_name, nv )
-	fdel = None
-	if delete:
-		def fdel(self):
-			try:
-				del factory(self)[annotation_name]
-			except KeyError:
-				if not delete_quiet:
-					raise
-
-	return property( fget, fset, fdel,
-					 doc=doc )
-
 import zope.deferredimport
 zope.deferredimport.initialize()
 zope.deferredimport.deprecatedFrom(
 	"Moved to nti.common",
-	"nti.common.property",
+	"nti.property.property",
 	"alias",
 	"read_alias",
 	"dict_alias",
@@ -69,4 +25,5 @@ zope.deferredimport.deprecatedFrom(
 	"CachedProperty",
 	"readproperty",
 	"_Lazy",
-	"_CachedProperty")
+	"_CachedProperty",
+	"annotation_alias")
