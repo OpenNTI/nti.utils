a5de57892775552c3e2f36996e7e566459f783fa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-08-23 15:26:34 +0000

    don't re/export common funcs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@95140 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/__init__.py
+++ src/nti/utils/__init__.py
@@ -11,13 +11,3 @@ logger = __import__('logging').getLogger(__name__)
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
-
-from nti.common.gravatar import create_gravatar_url
-
-from nti.common.gravatar import _AVATAR_SERVICES
-from nti.common.gravatar import KNOWN_GRAVATAR_TYPES
-from nti.common.gravatar import GENERATED_GRAVATAR_TYPES
-
-from nti.common.chameleon import setupChameleonCache
-
-from nti.zodb.common import make_cache_dir 
