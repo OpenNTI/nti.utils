8216230b4272ea2dcdd172e356fa15bdcf053c23
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-08 03:44:45 +0000

    use TextLine
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@108232 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/interfaces.py
+++ src/nti/utils/interfaces.py
@@ -9,16 +9,18 @@ __docformat__ = "restructuredtext en"
 
 from zope import interface
 
-from nti.schema.field import ValidTextLine
+from nti.schema.field import TextLine as ValidTextLine
+
 
 class ILDAP(interface.Interface):
-	ID = ValidTextLine(title="LDAP identifier", required=True)
-	URL = ValidTextLine(title="LDAP URL", required=True)
-	Username = ValidTextLine(title="Bind username", required=True)
-	Password = ValidTextLine(title="Bind password flag", required=True)
-	BaseDN = ValidTextLine(title="Base DN", required=False)
-	BackupURL = ValidTextLine(title="Backup LDAP URL", required=False)
+    ID = ValidTextLine(title="LDAP identifier", required=True)
+    URL = ValidTextLine(title="LDAP URL", required=True)
+    Username = ValidTextLine(title="Bind username", required=True)
+    Password = ValidTextLine(title="Bind password flag", required=True)
+    BaseDN = ValidTextLine(title="Base DN", required=False)
+    BackupURL = ValidTextLine(title="Backup LDAP URL", required=False)
+
 
 class IOAuthKeys(interface.Interface):
-	APIKey = ValidTextLine(title="API Key", required=True)
-	SecretKey = ValidTextLine(title="Secret Key", required=True)
+    APIKey = ValidTextLine(title="API Key", required=True)
+    SecretKey = ValidTextLine(title="Secret Key", required=True)
