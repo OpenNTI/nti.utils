3a7fa2e38f13b347230b50a294efbe8a89c324fb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-20 14:45:18 +0000

    bump nameparser;logilab-common; clean up code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@109175 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/nameparser.py
+++ src/nti/utils/nameparser.py
@@ -9,35 +9,35 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import nameparser
-nameparser_config = getattr(nameparser, "config")
-version_info = getattr(nameparser, 'VERSION', (0, 3, 16))
+from nameparser.config import prefixes
+from nameparser.config import suffixes
+from nameparser.config import Constants
 
-def suffixes():
-	return set(getattr(nameparser_config, 'SUFFIXES', None) or ())
 
 def suffix_acronyms():
-	return set(getattr(nameparser_config, 'SUFFIX_ACRONYMS', None) or ())
+    return suffixes.SUFFIX_ACRONYMS
+
 
 def suffix_not_acronyms():
-	return set(getattr(nameparser_config, 'SUFFIX_NOT_ACRONYMS', None) or ())
+    return suffixes.SUFFIX_NOT_ACRONYMS
 
-def all_prefixes():
-	return set(getattr(nameparser_config, 'PREFIXES', None) or ())
+
+def suffixes():
+    return suffix_acronyms() | suffix_not_acronyms()
+
 
 def all_suffixes():
-	return suffixes() | suffix_acronyms() | suffix_not_acronyms()
+    return suffixes() | suffix_acronyms() | suffix_not_acronyms()
+
+
+def all_prefixes():
+    return prefixes.PREFIXES
+
 
 def constants(prefixes=(), extra_suffixes=()):
-	prefixes = all_prefixes() if not prefixes else prefixes
-	extra_suffixes = () if not extra_suffixes else extra_suffixes
-	if version_info[0] == 0 and version_info[1] <= 3: # nameparser 0.3.X
-		v3_suffixes = suffixes() | set(extra_suffixes)
-		constants = nameparser_config.Constants(prefixes=prefixes, suffixes=v3_suffixes)
-	else:
-		not_acronyms = suffix_not_acronyms()
-		acronyms = suffix_acronyms() | set(extra_suffixes)
-		constants = nameparser_config.Constants(prefixes=prefixes,
-												suffix_acronyms=acronyms,
-												suffix_not_acronyms=not_acronyms)
-	return constants
+    not_acronyms = suffix_not_acronyms()
+    acronyms = suffix_acronyms() | set(extra_suffixes)
+    constants = Constants(prefixes=prefixes,
+                          suffix_acronyms=acronyms,
+                          suffix_not_acronyms=not_acronyms)
+    return constants
