c6e2b0444adc87e4312bd9d5029484b23f8a3087
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-22 19:22:31 +0000

    deprecate
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@109375 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/nameparser.py
+++ src/nti/utils/nameparser.py
@@ -9,35 +9,5 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nameparser.config import prefixes
-from nameparser.config import suffixes
-from nameparser.config import Constants
-
-
-def suffix_acronyms():
-    return suffixes.SUFFIX_ACRONYMS
-
-
-def suffix_not_acronyms():
-    return suffixes.SUFFIX_NOT_ACRONYMS
-
-
-def get_suffixes():
-    return suffix_acronyms() | suffix_not_acronyms()
-
-
-def all_suffixes():
-    return get_suffixes() | suffix_acronyms() | suffix_not_acronyms()
-
-
-def all_prefixes():
-    return prefixes.PREFIXES
-
-
-def constants(prefixes=(), extra_suffixes=()):
-    not_acronyms = suffix_not_acronyms()
-    acronyms = suffix_acronyms() | set(extra_suffixes)
-    constants = Constants(prefixes=prefixes,
-                          suffix_acronyms=acronyms,
-                          suffix_not_acronyms=not_acronyms)
-    return constants
+import zope.deprecation
+zope.deprecation.moved('nti.common.nameparser')
