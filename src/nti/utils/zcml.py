5fccca205a46616b7accebfe32617683f9dcbdcd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-31 23:25:29 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@110000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/zcml.py
+++ src/nti/utils/zcml.py
@@ -31,44 +31,49 @@ BASE_64 = u'base64'
 URL_QUOTE = u'urlquote'
 PASSWORD_ENCODING = (URL_QUOTE, BASE_64)
 
+
 class IRegisterLDAP(interface.Interface):
-	"""
-	The arguments needed for registering an ldap
-	"""
-	id = fields.TextLine(title="ldap identifier", required=True)
-	url = fields.TextLine(title="ldap url", required=True)
-	username = fields.TextLine(title="Bind username", required=True)
-	password = schema.Password(title="Bind password", required=True)
-	baseDN = fields.TextLine(title="Base DN", required=False)
-	encoding = fields.TextLine(title="Password encoding", required=False)
-	backupURL = fields.TextLine(title="ldap backup url", required=False)
-	
-def registerLDAP(_context, id, url, username, password, baseDN=None, 
-				 encoding=None, backupURL=None):
-	"""
-	Register an ldap
-	"""
-	encoding = encoding or u''
-	if encoding.lower() == URL_QUOTE:
-		password = urllib.unquote(password)
-	elif encoding.lower() == BASE_64:
-		password = base64.decodestring(password)
+    """
+    The arguments needed for registering an ldap
+    """
+    id = fields.TextLine(title="ldap identifier", required=True)
+    url = fields.TextLine(title="ldap url", required=True)
+    username = fields.TextLine(title="Bind username", required=True)
+    password = schema.Password(title="Bind password", required=True)
+    baseDN = fields.TextLine(title="Base DN", required=False)
+    encoding = fields.TextLine(title="Password encoding", required=False)
+    backupURL = fields.TextLine(title="ldap backup url", required=False)
 
-	factory = functools.partial(ldap.LDAP, ID=id, URL=url, Username=username,
-								Password=password, BaseDN=baseDN, 
-								BackupURL=backupURL)
 
-	utility(_context, provides=ILDAP, factory=factory, name=id)
+def registerLDAP(_context, id, url, username, password, baseDN=None,
+                 encoding=None, backupURL=None):
+    """
+    Register an ldap
+    """
+    encoding = encoding or u''
+    if encoding.lower() == URL_QUOTE:
+        password = urllib.unquote(password)
+    elif encoding.lower() == BASE_64:
+        password = base64.decodestring(password)
+
+    factory = functools.partial(ldap.LDAP, ID=id, URL=url, Username=username,
+                                Password=password, BaseDN=baseDN,
+                                BackupURL=backupURL)
+
+    utility(_context, provides=ILDAP, factory=factory, name=id)
 
 
 class IRegisterOAuthKeys(interface.Interface):
-	"""
-	The arguments needed for registering oauth keys
-	"""
-	id = fields.TextLine(title="ouath identifier", required=False, default='')
-	apiKey = fields.TextLine(title="API key", required=True)
-	secretKey = fields.TextLine(title="secrent key", required=True)
+    """
+    The arguments needed for registering oauth keys
+    """
+    id = fields.TextLine(title="ouath identifier", required=False, default='')
+    apiKey = fields.TextLine(title="API key", required=True)
+    secretKey = fields.TextLine(title="secrent key", required=True)
+
 
 def registerOAuthKeys(_context, apiKey, secretKey, id=u''):
-	factory = functools.partial(oauthkeys.OAuthKeys, APIKey=apiKey, SecretKey=secretKey)
-	utility(_context, provides=IOAuthKeys, factory=factory, name=id)
+    factory = functools.partial(oauthkeys.OAuthKeys,
+								APIKey=apiKey, 
+								SecretKey=secretKey)
+    utility(_context, provides=IOAuthKeys, factory=factory, name=id)
