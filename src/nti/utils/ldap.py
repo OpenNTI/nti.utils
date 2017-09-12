752491bdcac27c9e3d6c7b3aecdd860bef4131d0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-12 21:44:10 +0000

    use common meta directives
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.utils/trunk@121590 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/utils/ldap.py
+++ src/nti/utils/ldap.py
@@ -1,53 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Defines an ldap registration object
-
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
-
-from nti.base._compat import text_
-
-from nti.common.cypher import get_plaintext
-
-from nti.property.property import alias
-
-from nti.schema.eqhash import EqHash
-
-from nti.schema.fieldproperty import createDirectFieldProperties
-
-from nti.schema.schema import SchemaConfigured
-
-
-from nti.utils.interfaces import ILDAP
-
-
-@EqHash('ID')
-@interface.implementer(ILDAP)
-class LDAP(SchemaConfigured):
-    createDirectFieldProperties(ILDAP)
-
-    password = alias('Password')
-
-    @property
-    def id(self):
-        return self.ID
-
-    def __str__(self):
-        return self.URL
-
-    def __setattr__(self, name, value):
-        if name in ("Password", "password"):
-            try:
-                key = get_plaintext(value)
-                value = text_(key)
-            except (TypeError, StandardError):
-                pass
-        return SchemaConfigured.__setattr__(self, name, value)
+import zope.deprecation
+zope.deprecation.moved('nti.common.model')
--- src/nti/utils/oauthkeys.py
+++ src/nti/utils/oauthkeys.py
@@ -4,48 +4,10 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
-
-from nti.base._compat import text_
-
-from nti.common.cypher import get_plaintext
-
-from nti.property.property import alias
-
-from nti.schema.eqhash import EqHash
-
-from nti.schema.fieldproperty import createDirectFieldProperties
-
-from nti.schema.schema import SchemaConfigured
-
-from nti.utils.interfaces import IOAuthKeys
-
-
-@EqHash('APIKey', 'SecretKey')
-@interface.implementer(IOAuthKeys)
-class OAuthKeys(SchemaConfigured):
-    createDirectFieldProperties(IOAuthKeys)
-
-    apiKey = alias('APIKey')
-    secretKey = alias('SecretKey')
-
-    @property
-    def id(self):
-        return self.APIKey
-
-    def __str__(self):
-        return self.APIKey
-
-    def __setattr__(self, name, value):
-        if name in ("apiKey", "APIKey", "secretKey", "SecretKey"):
-            try:
-                key = get_plaintext(value)
-                value = text_(key)
-            except (TypeError, StandardError):
-                pass
-        return SchemaConfigured.__setattr__(self, name, value)
+import zope.deprecation
+zope.deprecation.moved('nti.common.model')
